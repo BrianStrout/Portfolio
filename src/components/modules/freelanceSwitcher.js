2873f58dd56ec6aa1bc650ac231ac97826f908c5
Brian J. Strout <brianjstrout@gmail.com>
2023-03-09 19:34:18 -0500

    started styling layout of links for freelance

--- src/components/modules/freelanceSwitcher.js
+++ src/components/modules/freelanceSwitcher.js
@@ -13,10 +13,10 @@ const freelanceClicker = (e) => {
       words.innerHTML = `
       <div class="freelance-container">
       <h1 class="words-header">Time To Up Your Web Presence?</h1>
-      <h3 class="handy"><i>
+      <h3 class="handy under"><i>
     If it's time to create or rethink your web presence,  get help with keeping it updated, or gain some guidance for your path forward, it's time we talk.
     </i></h3><br>
-    <p id="words-para" class="handy">
+    <p id="words-para" class="handy ind">
     I take great joy in helping individuals, businesses, and small organizations evolve how they interact with the web on a budget that is within their means. Whether it's regarding building a site bound to catch an eye, increasing your engagement and lowering your bounce rate; helping with management and keeping your online presence current and crisp, or helping with your video projects, I look forward to the opportunity to chat and start a lasting business relationship.
     <br>
     </p></div>`;
@@ -25,30 +25,59 @@ const freelanceClicker = (e) => {
       words.innerHTML = ` <div class="freelance-container"><h1 id="words-header">Examples</h1>
     <p id="handy">
     <ul>
-
-    <li>
-    <div class="linkOutTo" id="fugi"></div>
-    <h2 class="words-header">Spreed Sheet Mobile Menu</h2>
-    <p class="handy">Does your restaurant, cafe, or bar want a web based menu system you can update on a google spreadsheet from your phone?  This is what it could look like. :)</p>
-    
-  </li>
-  <li>  <div class="linkOutTo" id="winter"></div>
-    <h2 class="words-header">Motion Based Landing Page</h2>
-    <p class="handy">Capture your audiences attention with a landing page that has animated elements made from your own art or photos</p>
+    <li class="grid2">
+      <div class="linkOutTo" id="fugi"></div>
+      <div>
+        <h2 class="words-header">Spreed Sheet Mobile Menu</h2>
+        <p class="handy">
+          Does your restaurant, cafe, or bar want a web based menu system you can
+          update on a google spreadsheet from your phone? This is what it could
+          look like. :)
+        </p>
+      </div>
     </li>
-  <li>  <div class="linkOutTo" id="mookie"></div>
-    <h2 class="words-header">Playfully introduce your shop</h2>
-    <p class="handy">Want to set a welcoming vibe, let's set that up with giving your page an actual fun intro and simple style</p>
+  
+    <li class="grid2">
+      <div class="linkOutTo" id="winter"></div>
+      <div>
+        <h2 class="words-header">Motion Based Landing Page</h2>
+        <p class="handy">
+          Capture your audiences attention with a landing page that has animated
+          elements made from your own art or photos
+        </p>
+      </div>
     </li>
-  <li>  <div class="linkOutTo" id="Masha"></div>
-    <h2 class="words-header">Proper Introductions</h2>
-    <p class="handy">Bring heads to a stop with elegant animation intros to display your fashion or personal modeling</p>
+    <li class="grid2">
+      <div class="linkOutTo" id="mookie"></div>
+      <div>
+        <h2 class="words-header">Playfully introduce your shop</h2>
+        <p class="handy">
+          Want to set a welcoming vibe, let's set that up with giving your page an
+          actual fun intro and simple style
+        </p>
+      </div>
     </li>
-    <li>  <div class="linkOutTo" id="Masha"></div>
-    <h2 class="words-header">Dynamic Landing Page</h2>
-    <p class="handy">Bring heads to a stop with elegant animation intros to display your fashion or personal modeling</p>
+    <li class="grid2">
+      <div class="linkOutTo" id="Masha"></div>
+      <div>
+        <h2 class="words-header">Proper Introductions</h2>
+        <p class="handy">
+          Bring heads to a stop with elegant animation intros to display your
+          fashion or personal modeling
+        </p>
+      </div>
     </li>
-</ul>
+    <li class="grid2">
+      <div class="linkOutTo" id="Masha"></div>
+      <div>
+        <h2 class="words-header">Dynamic Landing Page</h2>
+        <p class="handy">
+          Bring heads to a stop with elegant animation intros to display your
+          fashion or personal modeling
+        </p>
+      </div>
+    </li>
+  </ul>
     </p></div>`;
       break;
     case "ball3":
